1eaacc016acc492538b3caad4bb096682eb92358
anajsh <anjash261@gmail.com>
2025-07-24 12:46:59 +0700

    Updated config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://smkmuhammadiyah2kalirejo.up.railway.app/api/v1';
+export const API_BASE_URL = 'https://smkmudakalirejo.pagekite.me/api/v1';
 export const IMAGE_BASE_URL = 'https://pub-5e4bcde41b9c4448ab189ac2f8100325.r2.dev';
 
 export const getProfileImageUrl = (profilePictureUrl: string | null): string | null => {
