a3aa10b0c8b1dd2ddf346d54715240ab6f990918
samskuyd <samskuydin@gmail.com>
2025-07-25 19:18:36 +0700

    Updated ProfileManagement.tsx

--- src/components/ProfileManagement.tsx
+++ src/components/ProfileManagement.tsx
@@ -221,7 +221,7 @@ const ProfileManagement: React.FC = () => {
       </div>
 
       {/* Main Content */}
-      <div className="flex-1 flex flex-col min-w-0 lg:ml-80">
+      <div className="flex-1 flex flex-col min-w-0">
         {/* Header */}
         <header className="bg-white border-b border-gray-200 px-6 py-4 sticky top-0 z-30">
           <div className="flex items-center justify-between">
