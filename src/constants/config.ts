dc978bb85f5eecd1a52be7bf871fdc47851c9406
hermannug <hermannugraha745@gmail.com>
2025-07-30 01:30:00 +0700

    Updated config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL = 'https://smkmudakalirejo.pagekite.me/api/v1';
-export const IMAGE_BASE_URL = 'https://pub-5e4bcde41b9c4448ab189ac2f8100325.r2.dev';
+export const IMAGE_BASE_URL = 'https://cdn.cigarverse.space';
 
 export const getProfileImageUrl = (profilePictureUrl: string | null): string | null => {
   if (!profilePictureUrl) return null;
