5149eff4dba8dc4e69ff102b57e56f73823bedb4
anwarsupr <anwarsupriyadi74@gmail.com>
2025-07-29 15:52:47 +0700

    Updated TeacherDashboard.tsx

--- src/components/dashboards/TeacherDashboard.tsx
+++ src/components/dashboards/TeacherDashboard.tsx
@@ -65,7 +65,7 @@ const TeacherDashboard: React.FC = () => {
       />
 
       {/* Main Content */}
-      <div className="flex-1 flex flex-col lg:ml-64">
+      <div className="flex-1 flex flex-col lg:ml-64 overflow-hidden">
         {/* Header Component */}
         <TeacherHeader
           user={user}
