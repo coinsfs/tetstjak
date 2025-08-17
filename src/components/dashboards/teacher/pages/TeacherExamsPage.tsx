7d3f0af68dc6feae3d5e97b6fd1df6d24070e502
rizkyadb <rizki.adityapramana21@gmail.com>
2025-08-17 11:32:55 +0700

    Updated TeacherExamsPage.tsx

--- src/components/dashboards/teacher/pages/TeacherExamsPage.tsx
+++ src/components/dashboards/teacher/pages/TeacherExamsPage.tsx
@@ -462,7 +462,7 @@ const TeacherExamsPage: React.FC = () => {
                     </td>
                     
                     {/* Aksi */}
-                    <td className="px-6 py-4 whitespace-nowrap text-center text-end">
+                    <td className="px-6 py-4 whitespace-nowrap text-center text-right">
                       <div className="flex items-center justify-center space-x-2">
                         {/* Action Buttons */}
                         {getActionButtons(exam).map((button, index) => (
