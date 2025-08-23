a9ac37f3b906cfe644b35eb114408f0f4860edcf
darmin99 <darminuhuy99@gmail.com>
2025-08-23 23:23:42 +0700

    Updated config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://54.179.214.145/api/v1';
+export const API_BASE_URL = 'https://testing.cigarverse.space/api/v1';
 export const IMAGE_BASE_URL = 'https://cdn.cigarverse.space';
 
 export const getProfileImageUrl = (profilePictureUrl: string | null): string | null => {
