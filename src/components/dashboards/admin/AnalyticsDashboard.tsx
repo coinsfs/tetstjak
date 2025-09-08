631453a509154410e16521e65f9e506652eae7db
anajsh <anjash261@gmail.com>
2025-09-08 19:00:53 +0700

    Updated AnalyticsDashboard.tsx

--- src/components/dashboards/admin/AnalyticsDashboard.tsx
+++ src/components/dashboards/admin/AnalyticsDashboard.tsx
@@ -111,7 +111,7 @@ const AnalyticsDashboard: React.FC = () => {
   ];
 
   return (
-    <div className="min-h-screen bg-gray-50 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gray-50">
       <div className="max-w-7xl mx-auto">
         {/* Header */}
         <div className="bg-white border border-gray-200 rounded-lg p-4 sm:p-6 mb-4 sm:mb-6">
